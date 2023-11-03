45e7641cbede1078a5e6be95f2116e7e4f6c2240
jantiegges <jantiegges@gmail.com>
2023-11-03 11:09:41 -0400

    implement nb model and grid search

---
+++ main.py
@@ -0,0 +1,46 @@
+import itertools
+import pandas as pd
+
+from utils.data_preparation import naive_bayes_preprocessing
+from models.naive_bayes import NaiveBayes
+
+def nb_grid_search(params):
+    results_list = []
+    param_combinations = itertools.product(params['alpha'], params['fit_prior'], params['use_bigrams'], params['remove_stopwords'])
+
+    for alpha, fit_prior, use_bigrams, remove_stopwords in param_combinations:
+        processed_data = naive_bayes_preprocessing(
+            remove_stopwords=remove_stopwords, 
+            use_bigrams=use_bigrams
+        )
+        X_train, y_train = processed_data['train']
+        X_val, y_val = processed_data['validation']
+        
+        nb = NaiveBayes(alpha=alpha, fit_prior=fit_prior)
+        nb.fit(X_train, y_train)
+        predictions = nb.predict(X_val)
+        acc = nb.evaluate_acc(y_val, predictions)
+        
+        results_list.append({
+            'alpha': alpha,
+            'fit_prior': fit_prior,
+            'use_bigrams': use_bigrams,
+            'remove_stopwords': remove_stopwords,
+            'val_accuracy': acc
+        })
+
+    results_df = pd.DataFrame(results_list)
+    results_df = results_df.sort_values(by='val_accuracy', ascending=False).reset_index(drop=True)
+    results_df.to_pickle('out/nb_grid_search_results.pkl')
+    return results_df
+
+if __name__ == '__main__':
+    params = {
+        'alpha': [0.0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0],
+        'fit_prior': [True, False],
+        'use_bigrams': [True, False],
+        'remove_stopwords': [True, False]
+    }
+    num_combinations = len(list(itertools.product(*params.values())))
+    print(f"The number of combinations is: {num_combinations}")
+    grid_search_results = nb_grid_search(params)
---
+++ models/naive_bayes.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class NaiveBayes:
+    def __init__(self, alpha=1.0, fit_prior=True):
+        self.alpha = alpha # smoothing (1.0 for Laplace smoothing)
+        self.fit_prior = fit_prior
+
+        self.class_log_prior_ = None
+        self.feature_log_prob_ = None
+
+    def fit(self, X, y):
+        n_classes = len(np.unique(y))
+        if self.fit_prior:
+            class_counts = np.bincount(y)
+            self.class_log_prior_ = np.log((class_counts + self.alpha) / (class_counts.sum() + n_classes * self.alpha))
+        else:
+            self.class_log_prior_ = np.log(np.full(n_classes, 1 / n_classes))
+
+        # calc the log likelihood for each feature in each class
+        feature_counts_per_class = np.zeros((n_classes, X.shape[1]))
+        for class_label in range(n_classes):
+            feature_counts_per_class[class_label] = X[y == class_label].sum(axis=0)
+        self.feature_log_prob_ = np.log((feature_counts_per_class + self.alpha) / 
+                                  (feature_counts_per_class.sum(axis=1, keepdims=True) + X.shape[1] * self.alpha))
+
+    def predict(self, X):
+        # calc the log likelihood for each class in each sample
+        log_likelihood = X @ self.feature_log_prob_.T + self.class_log_prior_
+        return log_likelihood.argmax(axis=1)
+    
+    def evaluate_acc(self, y, y_pred):
+        return (y == y_pred).mean()
