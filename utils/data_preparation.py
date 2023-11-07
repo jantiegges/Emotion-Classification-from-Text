9bc18fb9da3ae55eb6453557fcfb0058a3a925eb
rthane <rishabh.thaney@mail.mcgill.ca>
2023-11-06 21:03:18 -0500

    bert preprocessing function

--- utils/data_preparation.py
+++ utils/data_preparation.py
@@ -1,6 +1,9 @@
 from datasets import load_dataset
 from sklearn.feature_extraction.text import CountVectorizer
+from transformers import BertTokenizer
+from datasets import load_dataset
 import pandas as pd
+import torch
 
 def get_emotion_dataset():
     return load_dataset('dair-ai/emotion')
@@ -23,5 +26,26 @@ def naive_bayes_preprocessing(remove_stopwords=False, use_bigrams=False):
         y = df['label'].values
         
         processed_data[split] = (X, y)
-    
+
     return processed_data, vectorizer
+
+def bert_preprocessing():
+    
+    dataset = load_dataset('dair-ai/emotion')
+
+    # Initialize the BERT tokenizer
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    processed_data = {}
+    for split, data in dataset.items():
+        df = pd.DataFrame(data)
+
+        # Tokenize the text data using the BERT tokenizer
+        tokenized_inputs = tokenizer(df['text'].tolist(), padding='max_length', truncation=True, max_length=128, return_tensors='pt')
+        input_ids = tokenized_inputs['input_ids']
+        attention_mask = tokenized_inputs['attention_mask']
+        labels = torch.tensor(df['label'].values)
+
+        processed_data[split] = (input_ids, attention_mask, labels)
+
+    return processed_data, tokenizer
