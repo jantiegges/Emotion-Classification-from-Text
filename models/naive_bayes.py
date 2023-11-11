abae24e183004db52a6dd36be309422b4c081254
jantiegges <jantiegges@gmail.com>
2023-11-10 19:19:25 -0500

    correct naive bayes implementation

--- models/naive_bayes.py
+++ models/naive_bayes.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class NaiveBayes:
-    def __init__(self, alpha=1.0, fit_prior=True):
-        self.alpha = alpha # smoothing (1.0 for Laplace smoothing)
-        self.fit_prior = fit_prior
+    def __init__(self, alpha_prior=10, alpha_likelihood=1.0):
+        self.alpha_prior = alpha_prior
+        self.alpha_likelihood = alpha_likelihood
 
         self.class_log_prior_ = None
         self.feature_log_prob_ = None
@@ -11,18 +11,15 @@ class NaiveBayes:
     def fit(self, X, y):
         n_classes = len(np.unique(y))
         # compute the log prior for each class
-        if self.fit_prior:
-            class_counts = np.bincount(y)
-            self.class_log_prior_ = np.log((class_counts + self.alpha) / (class_counts.sum() + n_classes * self.alpha))
-        else:
-            self.class_log_prior_ = np.log(np.full(n_classes, 1 / n_classes))
+        class_counts = np.bincount(y)
+        self.class_log_prior_ = np.log((class_counts + self.alpha_prior) / (class_counts.sum() + n_classes * self.alpha_prior))
 
         # calc the log likelihood for each feature in each class
         feature_counts_per_class = np.zeros((n_classes, X.shape[1]))
         for class_label in range(n_classes):
             feature_counts_per_class[class_label] = X[y == class_label].sum(axis=0)
-        self.feature_log_prob_ = np.log((feature_counts_per_class + self.alpha) / 
-                                  (feature_counts_per_class.sum(axis=1, keepdims=True) + X.shape[1] * self.alpha))
+        self.feature_log_prob_ = np.log((feature_counts_per_class + self.alpha_likelihood) / 
+                                  (feature_counts_per_class.sum(axis=1, keepdims=True) + X.shape[1] * self.alpha_likelihood))
 
     def predict(self, X):
         # calc the log likelihood for each class in each sample
