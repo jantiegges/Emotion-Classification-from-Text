f694d578562631f73490bbb9b1d999759b11a65e
jonathan <jonathancolacocarr@gmail.com>
2023-11-14 21:05:03 -0500

    Restructure bert model and run hparam search

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from sklearn.metrics import accuracy_score
 
 from utils.data_preparation import naive_bayes_preprocessing, bert_preprocessing
 from models.naive_bayes import NaiveBayes
+from models.bert import Bert
 
+SEED = 42
+torch.manual_seed(SEED)
 
 def nb_grid_search(params):
     results_list = []
@@ -42,6 +45,47 @@ def nb_grid_search(params):
     results_df.to_pickle('out/nb_grid_search_results.pkl')
     return results_df
 
+def bert_grid_search(params, exp_name):
+
+    ## Data Preparation ##
+    bert_processed_data, tokenizer = bert_preprocessing()
+    input_ids_train, attention_mask_train, y_train = bert_processed_data['train']
+    input_ids_val, attention_mask_val, y_val = bert_processed_data['validation']
+    input_ids_test, attention_mask_test, y_test = bert_processed_data['test']
+
+    train_dataset = TensorDataset(input_ids_train, attention_mask_train, y_train)
+    val_dataset = TensorDataset(input_ids_val, attention_mask_val, y_val)
+    test_dataset = TensorDataset(input_ids_test, attention_mask_test, y_test)
+
+    results_list = []
+    param_combinations = itertools.product(params['epochs'], params['batch_size'], params['lr'], params['weight_decay'],
+                                           params['fine_tune_last_layers'])
+
+    for epochs, batch_size, lr, weight_decay, fine_tune_last_layers in param_combinations:
+
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        train_loader_eval = DataLoader(train_dataset, batch_size=batch_size)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+        bert = Bert(fine_tune_last_layers=fine_tune_last_layers)
+        bert.fit(train_loader, train_loader_eval, val_loader, y_train, y_val, epochs, lr, weight_decay)
+        y_pred = bert.predict(val_loader)
+        val_accuracy = accuracy_score(y_val, y_pred)
+
+        results_list.append({
+            'epochs': epochs,
+            'batch_size': batch_size,
+            'lr': lr,
+            'weight_decay': weight_decay,
+            'fine_tune_last_layers': fine_tune_last_layers,
+            'val_accuracy': val_accuracy
+        })
+
+    results_df = pd.DataFrame(results_list)
+    results_df = results_df.sort_values(by='val_accuracy', ascending=False).reset_index(drop=True)
+    results_df.to_pickle(f'out/_grid_search_results_{exp_name}.pkl')
+    return results_df
 
 def bert_train(train_loader, train_loader_eval, val_loader, epochs, lr, fine_tune_last_layers=False):
     """Fine-tune BERT for emotion classification"""
@@ -139,15 +183,28 @@ def bert_predict(model, data_loader, return_attentions=False):
 if __name__ == '__main__':
 
     ### Naive Bayes Grid Search ###
+    #params = {
+    #    'alpha_prior': [0, 1, 10, 100, 1000, 10000, 100000],
+    #    'alpha_likelihood': [0.0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0],
+    #    'use_bigrams': [True, False],
+    #    'remove_stopwords': [True, False]
+    #}
+    #num_combinations = len(list(itertools.product(*params.values())))
+    #print(f"The number of combinations is: {num_combinations}")
+    #grid_search_results = nb_grid_search(params)
+
+    ### BERT Grid search - last layers only ###
     params = {
-        'alpha_prior': [0, 1, 10, 100, 1000, 10000, 100000],
-        'alpha_likelihood': [0.0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0],
-        'use_bigrams': [True, False],
-        'remove_stopwords': [True, False]
+        'epochs': [3],
+        'batch_size': [32, 64, 128],
+        'lr': [1e-5, 2e-5, 3e-5],
+        'weight_decay': [0.01, 0.001],
+        'fine_tune_last_layers': [True]
     }
     num_combinations = len(list(itertools.product(*params.values())))
     print(f"The number of combinations is: {num_combinations}")
-    grid_search_results = nb_grid_search(params)
+    grid_search_results = bert_grid_search(params, exp_name='last_layers')
+    exit()
 
     ### BERT ###
 
