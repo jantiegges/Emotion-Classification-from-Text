edf601d0f3f5e187057dc1ec6c6e4749a35d82b2
jonathan <jonathancolacocarr@gmail.com>
2023-11-15 11:08:53 -0500

    Put prior params in fit

--- models/naive_bayes.py
+++ models/naive_bayes.py
@@ -1,25 +1,22 @@
 import numpy as np
 
 class NaiveBayes:
-    def __init__(self, alpha_prior=10, alpha_likelihood=1.0):
-        self.alpha_prior = alpha_prior
-        self.alpha_likelihood = alpha_likelihood
-
+    def __init__(self):
         self.class_log_prior_ = None
         self.feature_log_prob_ = None
 
-    def fit(self, X, y):
+    def fit(self, X, y, alpha_prior=10, alpha_likelihood=1.0):
         n_classes = len(np.unique(y))
         # compute the log prior for each class
         class_counts = np.bincount(y)
-        self.class_log_prior_ = np.log((class_counts + self.alpha_prior) / (class_counts.sum() + n_classes * self.alpha_prior))
+        self.class_log_prior_ = np.log((class_counts + alpha_prior) / (class_counts.sum() + n_classes * alpha_prior))
 
         # calc the log likelihood for each feature in each class
         feature_counts_per_class = np.zeros((n_classes, X.shape[1]))
         for class_label in range(n_classes):
             feature_counts_per_class[class_label] = X[y == class_label].sum(axis=0)
-        self.feature_log_prob_ = np.log((feature_counts_per_class + self.alpha_likelihood) / 
-                                  (feature_counts_per_class.sum(axis=1, keepdims=True) + X.shape[1] * self.alpha_likelihood))
+        self.feature_log_prob_ = np.log((feature_counts_per_class + alpha_likelihood) /
+                                  (feature_counts_per_class.sum(axis=1, keepdims=True) + X.shape[1] * alpha_likelihood))
 
     def predict(self, X):
         # calc the log likelihood for each class in each sample
